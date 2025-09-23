a67a97be23ecb148db4bf66e8aec5513ad188925
mahmoud-zino <mahmoud.alhalaby@gmail.com>
2025-09-23 23:09:27 +0200

    implement logout and nav-menu

--- myphysio/src/lib/components/NavBar.svelte
+++ myphysio/src/lib/components/NavBar.svelte
@@ -1,31 +1,49 @@
 <script lang="ts">
     import { Navigation } from '@skeletonlabs/skeleton-svelte';
-    const mdScreenSize = 767;
-    let windowInnerWidth: number = $state(0);
+    import { Calendar, User, ReceiptEuro, Stethoscope, Settings, LogOut } from '@lucide/svelte';
+    import { page } from '$app/state';
+	  import { isMobile } from '$lib/stores/breakpoint';
 
-    $inspect(windowInnerWidth);
+    let value = $state('Kalendar');
+
+    const navInfo = [
+      { label: 'Kalendar', href: '/app/calendar', icon: Calendar },
+      { label: 'Kunde', href: '/app/customer', icon: User },
+      { label: 'Rechnung', href: '/app/bill', icon: ReceiptEuro },
+      { label: 'Behandlung', href: '/app/treatment', icon: Stethoscope },
+      { label: 'Einstellungen', href: '/app/settings', icon: Settings, classes:'mt-auto' }
+    ];
+    const navFooterInfo = [
+      { label: 'Abmelden', href: '/auth/logout', icon: LogOut }
+    ];
 </script>
-<svelte:window bind:innerWidth={windowInnerWidth}/>
 
-{#if windowInnerWidth > mdScreenSize}
-    <Navigation.Rail expanded={true}>
-    {#snippet tiles()}
-      <Navigation.Tile labelExpanded="Browse Files" href="#/files">
-      </Navigation.Tile>
-      <Navigation.Tile labelExpanded="Browse Images" href="#/images">
-      </Navigation.Tile>
-      <Navigation.Tile labelExpanded="Browse Music" href="#/music">
-      </Navigation.Tile>
-      <Navigation.Tile labelExpanded="Browse Videos" href="#/videos">
-      </Navigation.Tile>
-      <Navigation.Tile labelExpanded="Browse Games" href="/games">
-      </Navigation.Tile>
-    {/snippet}
-    {#snippet footer()}
-        <Navigation.Tile labelExpanded="Settings" href="/settings" title="Settings">
-        </Navigation.Tile>
-    {/snippet}
-  </Navigation.Rail>
+{#if $isMobile}
+  <Navigation.Bar {value} onValueChange={(newValue) => (value = newValue)}>
+    {#each navInfo as {label, href, icon}}
+    {@const Icon = icon}
+    <Navigation.Tile id={label} label={label} {href} title={label} selected={page.url.pathname.startsWith(href)}>
+      <Icon />
+    </Navigation.Tile>
+    {/each}
+  </Navigation.Bar>
 {:else}
-test
+ <Navigation.Rail expanded={true} widthExpanded="w-48" tilesJustify="start" footerClasses="hr">
+  {#snippet tiles()}
+    {#each navInfo as {label, href, icon, classes}}
+    {@const Icon = icon}
+    <Navigation.Tile labelExpanded={label} {href} title={label} selected={page.url.pathname.startsWith(href)} expandedClasses={classes}>
+      <Icon />
+    </Navigation.Tile>
+    {/each}
+  {/snippet}
+  {#snippet footer()}
+    {#each navFooterInfo as {label, href, icon}}
+    {@const Icon = icon}
+    <Navigation.Tile labelExpanded={label} {href} title={label} selected={page.url.pathname.startsWith(href)}>
+      <Icon />
+    </Navigation.Tile>
+    {/each}
+  {/snippet}
+  </Navigation.Rail>
 {/if}
--- myphysio/src/routes/app/+layout.svelte
+++ myphysio/src/routes/app/+layout.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
+    import favIconUrl from '$lib/assets/favicon.svg';
 	import NavBar from '$lib/components/NavBar.svelte';
 </script>
 
-<!-- screen -->
-<div class="grid h-screen grid-rows-[auto_1fr_auto]">
-	<!-- header -->
-	<header class="p-4 flex justify-center items-center bg-red-600">header</header>
-	<!-- screen middle -->
-  	<div class="grid grid-rows-[auto_1fr] md:grid-rows-none md:grid-cols-[auto_1fr] min-h-0">
-		<!-- Sidebar -->
-		<aside class="p-4">
-			<NavBar />
-		</aside>
-		<!-- Main Content -->
-		<main class="p-4 bg-green-600">
-            <slot />
-		</main>
-	</div>
+<div class="grid h-screen grid-rows-[auto_1fr]">
+  <header class="p-4 flex justify-center items-center bg-surface-100-900">
+    <div class="flex items-center justify-center space-x-4">
+      <img src={favIconUrl} alt="Logo" class="w-10 h-10" />
+      <h2 class="h2"><span class="text-primary-500">My</span>Physio</h2>
+    </div>
+  </header>
+
+  <div class="grid min-h-0 md:grid-cols-[auto_1fr]">
+    <aside class="fixed inset-x-0 bottom-0 z-20 md:static">
+      <NavBar />
+    </aside>
+
+    <main class="order-1 md:order-2 p-4 min-h-0 overflow-auto pb-16 md:pb-0">
+      <slot />
+    </main>
+  </div>
 </div>
